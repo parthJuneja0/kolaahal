a99d828bb15e4b17f31a94287ece01b600802a38
parthjuneja040 <parthjuneja040@gmail.com>
2025-03-05 22:36:44 +0530

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div className="">
-      video
+    <div className="relative w-full h-screen">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/assets/bg.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white bg-black/50">
+        <h1 className="text-8xl font-bold tracking-wide">Kolahal</h1>
+        <p className="text-2xl mt-2">Where chaos meets creativity</p>
+      </div>
     </div>
   );
 }
