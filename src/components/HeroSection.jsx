4769261dca55f6e4484a002972e711054e68e463
parthjuneja040 <parthjuneja040@gmail.com>
2025-03-22 15:03:10 +0530

    register button remove

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -139,20 +139,6 @@ const HeroSection = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <motion.button
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{
-                  delay: 1.1,
-                  duration: 0.5,
-                  type: "spring",
-                }}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                className="px-6 sm:px-8 py-2 sm:py-3 bg-amber-200 rounded-full border border-amber-300 text-black cursor-pointer hover:scale-105 transition-all duration-300"
-              >
-                Register Now
-              </motion.button>
             </Link>
           </div>
         </div>
